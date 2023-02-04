ae95a13a4be1242fd71a1cc16f5f853b536d0fce
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-02-04 14:43:17 +0300

    SpotBugs Added

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,6 +8,7 @@ public class StatisticsService {
      * @return - index of first max value
      */
     public long findMax(long[] incomes) {
+        //int forBug [] = {2, 5, 8}; bug for spotbugs
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
